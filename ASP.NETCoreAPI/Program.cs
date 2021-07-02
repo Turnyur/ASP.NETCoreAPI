ab3f45eaac5a74a6c354cbd52cd3b64f6b3e241b
Turnyur <chukunah@gmail.com>
2021-07-02 11:49:07 +0100

    Adds logging using Serilog to API

--- ASP.NETCoreAPI/Program.cs
+++ ASP.NETCoreAPI/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +16,35 @@ namespace ASP.NETCoreAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.
+                File(
+                path: "C:\\Users\\devch\\logs\\ASP.NETCoreAPI\\logs-.txt",
+               outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u3}] {Message:lj}{NewLine}{Exception}",
+               rollingInterval: RollingInterval.Day,
+               restrictedToMinimumLevel: LogEventLevel.Information
+                ).
+                CreateLogger();
+            try
+            {
+                Log.Information("ASP.NET CoreAPI started....");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Application couldn't start correctly ");
+
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+           
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
